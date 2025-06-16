513871e699a05495c7e22f04487908861dbc86cd
theestdtrader <merakimeloncagez22@gmail.com>
2025-06-16 14:18:25 +0200

    Update FuturesTable.tsx

--- src/components/FuturesTable.tsx
+++ src/components/FuturesTable.tsx
@@ -89,7 +89,7 @@ const FuturesTable: React.FC<FuturesTableProps> = ({ onGetPlan }) => {
       area: "Inactivity Period",
       assessment: "Phase 1-4 14 days | *Live Funded 7 days",
 
-      funded: "Phase 1-4 14 days | Live Funded 7 days",
+      funded: "Phase 1-4; 14 days | Live Funded 7 days",
       notes: "Must place trade (Hard Breach)",
       highlighted: false,
     },
